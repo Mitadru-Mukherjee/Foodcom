8cc35a7c1af8b649df08bfa43d526444d4c8d5a0
Mitadru-Mukherjee <mitadru727@gmail.com>
2023-05-31 12:37:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from "./components/Hero";
+import Main from "./components/Main";
+
+
+export default function App(){
+    return(
+        <div>
+            <Hero/>
+            <Main/>
+        </div>
+    )
+};
